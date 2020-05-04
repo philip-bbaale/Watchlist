b11b91dd47f6f42f3dfc772b5d5548f0b3b07f8a
Pip Bbaale <namugera.philip@gmail.com>
2020-05-04 12:49:37 +0300

    adding profile pic

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -10,6 +12,8 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
@@ -27,11 +31,16 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
      # setting config
     from .requests import configure_request
     configure_request(app)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    
 
     return app
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgres://gsapdsysaudhdt:1a505e7763ceec04f1b563b24b24c5b5681104b5d03dce8fa973c5c2bda26fe5@ec2-52-86-73-86.compute-1.amazonaws.com:5432/d3okpr2s0frj1h'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     pass
