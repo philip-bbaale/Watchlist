765ae20dbd79642a870464996516ab6b240695a6
Pip Bbaale <namugera.philip@gmail.com>
2020-05-04 12:11:34 +0300

    adding user profiles

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..requests import get_movies,get_movie,search_movie
-from .forms import ReviewForm
-from ..models import Review
+from .forms import ReviewForm,UpdateProfile
+from ..models import Review, User
+from flask_login import login_required
+from .. import db
+
 
 
 # Views
@@ -55,6 +58,7 @@ def search(movie_name):
 
 
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
 def new_review(id):
 
     form = ReviewForm()
@@ -72,3 +76,31 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class Config:
 
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgres://gsapdsysaudhdt:1a505e7763ceec04f1b563b24b24c5b5681104b5d03dce8fa973c5c2bda26fe5@ec2-52-86-73-86.compute-1.amazonaws.com:5432/d3okpr2s0frj1h'
 
 class ProdConfig(Config):
     pass
